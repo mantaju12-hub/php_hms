d6ac623fe88ed8a5d03f6e857d7ca02d245b3cef
mantaju12-hub <mantaju30@gmail.com>
2025-04-23 11:49:12 +0100

    first upload

---
+++ hospital/hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+			<span class="text-bold text-uppercase"> HMS To Hakim Gizaw General Hospital</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
